c13896521133886c4c6cf0ef4c4b47d35c19b46a
SL25 <songlei25@huawei.com>
2025-11-02 17:25:37 +0800

    [AscendNPU IR]: fix: Support tracing back multi values including yield values

--- bishengir/lib/Dialect/Utils/Util.cpp
+++ bishengir/lib/Dialect/Utils/Util.cpp
@@ -46,6 +46,87 @@ namespace mlir {
 
 namespace {
 
+SmallVector<Value> tracebackValuesImpl(Value memrefVal) {
+  // case 1: v is the iter_arg of a scf.for
+  SmallVector<Value> result;
+  if (auto arg = dyn_cast<BlockArgument>(memrefVal)) {
+    if (arg.getParentRegion() == nullptr) {
+      return result;
+    }
+    if (auto forOp =
+            dyn_cast<scf::ForOp>(arg.getParentRegion()->getParentOp())) {
+      if (arg.getArgNumber() > 0 &&
+          forOp.getInitArgs().size() > arg.getArgNumber() - 1) {
+        result.emplace_back(forOp.getInitArgs()[arg.getArgNumber() - 1]);
+        result.emplace_back(forOp.getYieldedValues()[arg.getArgNumber() - 1]);
+      }
+    }
+    if (auto whileOp =
+            dyn_cast<scf::WhileOp>(arg.getParentRegion()->getParentOp())) {
+      result.emplace_back(whileOp.getTiedLoopInit(arg)->get());
+    }
+  }
+
+  Operation *def = memrefVal.getDefiningOp();
+  if (!def) {
+    // failed to trace back
+    return result;
+  }
+
+  // case 2: v is the result of cast-like ops
+  //  - memref.cast
+  //  - memref.collapse_shape
+  //  - memref.expand_shape
+  //  - memref.memory_space_cast
+  //  - memref.reinterpret_cast
+  //  - memref.reshape
+  //  - memref.transpose
+  if (auto op = dyn_cast<memref::CastOp>(def)) {
+    result.emplace_back(op.getSource());
+  } else if (auto op = dyn_cast<memref::CollapseShapeOp>(def)) {
+    result.emplace_back(op.getSrc());
+  } else if (auto op = dyn_cast<memref::ExpandShapeOp>(def)) {
+    result.emplace_back(op.getSrc());
+  } else if (auto op = dyn_cast<memref::MemorySpaceCastOp>(def)) {
+    result.emplace_back(op.getSource());
+  } else if (auto op = dyn_cast<memref::ReinterpretCastOp>(def)) {
+    result.emplace_back(op.getSource());
+  } else if (auto op = dyn_cast<memref::ReshapeOp>(def)) {
+    result.emplace_back(op.getSource());
+  } else if (auto op = dyn_cast<memref::TransposeOp>(def)) {
+    result.emplace_back(op.getIn());
+  } else if (auto op = dyn_cast<UnrealizedConversionCastOp>(def)) {
+    result.emplace_back(
+        op.getOperand(cast<OpResult>(memrefVal).getResultNumber()));
+  } else if (auto op = dyn_cast<scf::ForOp>(def)) {
+    // trace back memref.alloc support scf.for
+    result.emplace_back(
+        op.getInitArgs()[cast<OpResult>(memrefVal).getResultNumber()]);
+    result.emplace_back(
+        op.getYieldedValues()[cast<OpResult>(memrefVal).getResultNumber()]);
+  } else if (auto op = dyn_cast<scf::IfOp>(def)) {
+    result.emplace_back(op.thenYield()->getOperand(
+        cast<OpResult>(memrefVal).getResultNumber()));
+    result.emplace_back(op.elseYield()->getOperand(
+        cast<OpResult>(memrefVal).getResultNumber()));
+  }
+
+  if (!result.empty()) {
+    return result;
+  }
+
+  // case 3: v is the result of the view-like ops
+  //  - memref::view
+  //  - memref::subview
+  if (auto op = dyn_cast<memref::ViewOp>(def)) {
+    result.emplace_back(op.getViewSource());
+  } else if (auto op = dyn_cast<memref::SubViewOp>(def)) {
+    result.emplace_back(op.getViewSource());
+  }
+
+  return result;
+}
+
 Value tracebackImpl(Value memrefVal) {
   // case 1: v is the iter_arg of a scf.for
   Value result;
@@ -860,15 +941,27 @@ bool utils::areShapesAligned(ArrayRef<int64_t> staticShapes,
   return true;
 }
 
-Value utils::tracebackMemRef(Value memrefVal) {
+SmallVector<Value> utils::tracebackMemRefVec(Value memrefVal) {
+  SmallVector<Value> memrefValues;
+  memrefValues.push_back(memrefVal);
   int loopBound = 256;
-  while (memrefVal && !utils::isAllocLikeOp(memrefVal)) {
-    auto upward = tracebackImpl(memrefVal);
-    if (!upward) {
+  while (!memrefValues.empty() &&
+         std::any_of(memrefValues.begin(), memrefValues.end(),
+                     [](Value &val) { return !utils::isAllocLikeOp(val); })) {
+    Value allocVal;
+    for (auto val : memrefValues) {
+      if (!utils::isAllocLikeOp(val)) {
+        allocVal = val;
+        break;
+      }
+    }
+    auto upward = tracebackValuesImpl(allocVal);
+    if (upward.empty()) {
       break;
     }
-
-    memrefVal = upward;
+    auto it = std::find(memrefValues.begin(), memrefValues.end(), allocVal);
+    memrefValues.erase(it);
+    memrefValues.append(upward.begin(), upward.end());
 
     // avoid infinite loop
     if (loopBound-- < 0) {
@@ -877,20 +970,45 @@ Value utils::tracebackMemRef(Value memrefVal) {
       break;
     }
   }
-
-  return memrefVal;
+  return memrefValues;
 }
 
-Value utils::tracebackMemRef(Value memrefVal,
-                             std::function<bool(Value)> targetFn) {
+Value utils::tracebackMemRef(Value memrefVal) {
+  SmallVector<Value> memrefValues = utils::tracebackMemRefVec(memrefVal);
+  if (memrefValues.empty()) {
+    return memrefVal;
+  }
+  if (memrefValues.size() > 1) {
+    assert(memrefVal.getDefiningOp());
+    memrefVal.getDefiningOp()->emitError(
+        "tracebackMemRef found multiple sources!");
+  }
+  return memrefValues[0];
+}
+
+SmallVector<Value>
+utils::tracebackMemRefToTargetVec(Value memrefVal,
+                                  std::function<bool(Value)> targetFn) {
+  SmallVector<Value> memrefValues;
+  memrefValues.push_back(memrefVal);
   int loopBound = 256;
-  while (memrefVal && !targetFn(memrefVal)) {
-    auto upward = tracebackImpl(memrefVal);
-    if (!upward) {
+  while (!memrefValues.empty() &&
+         std::any_of(memrefValues.begin(), memrefValues.end(),
+                     [&targetFn](Value &val) { return !targetFn(val); })) {
+    Value allocVal;
+    for (auto val : memrefValues) {
+      if (!targetFn(val)) {
+        allocVal = val;
+        break;
+      }
+    }
+    auto upward = tracebackValuesImpl(allocVal);
+    if (upward.empty()) {
       break;
     }
-
-    memrefVal = upward;
+    auto it = std::find(memrefValues.begin(), memrefValues.end(), allocVal);
+    memrefValues.erase(it);
+    memrefValues.append(upward.begin(), upward.end());
 
     // avoid infinite loop
     if (loopBound-- < 0) {
@@ -899,8 +1017,7 @@ Value utils::tracebackMemRef(Value memrefVal,
       break;
     }
   }
-
-  return memrefVal;
+  return memrefValues;
 }
 
 std::optional<memref::AllocOp> utils::tracebackMemRefToAlloc(Value memrefVal) {
