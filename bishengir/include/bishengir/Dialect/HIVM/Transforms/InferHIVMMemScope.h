e3bcc134e9e7643dff3a2ddb4e97f2e116383295
lu2038 <luwei789@huawei.com>
2025-11-10 19:54:10 +0800

    fix: hivm-infer-mem-scope, set the alloc mem to L1 for aic when the mem not used by hivmop

--- bishengir/include/bishengir/Dialect/HIVM/Transforms/InferHIVMMemScope.h
+++ bishengir/include/bishengir/Dialect/HIVM/Transforms/InferHIVMMemScope.h
@@ -54,8 +54,8 @@ LogicalResult inferAndPropagateMemScopeForFunc(func::FuncOp op);
 LogicalResult inferAndPropagateMemScopeForPointerCast(hivm::PointerCastOp op);
 
 /// Infer, propagate, and set memory scope information to AllocOp.
-/// \note Set alloc memory scope to ub.
-LogicalResult inferAndPropagateUbufMemScope(memref::AllocOp allocOp);
+/// \note Set alloc which not used by HIVM op memory scope to L1. And set other alloc memory scope to ub
+LogicalResult inferAndPropagateMemScopeForAlloc(memref::AllocOp op, hivm::AddressSpace space);
 
 } // namespace hivm
 } // namespace mlir
