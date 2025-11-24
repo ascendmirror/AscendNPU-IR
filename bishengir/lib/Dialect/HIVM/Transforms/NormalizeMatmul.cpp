c185e69a59096e9797c168db7dd3328f8eb7f8b7
dainbow <suslov.pavel1@huawei.com>
2025-11-24 17:30:56 +0800

    [AscendNPU IR]: fix: added cross loop pattern for insert load store
    Co-authored-by: Suslov Pavel<suslov.pavel1@huawei.com>

--- bishengir/lib/Dialect/HIVM/Transforms/NormalizeMatmul.cpp
+++ bishengir/lib/Dialect/HIVM/Transforms/NormalizeMatmul.cpp
@@ -98,7 +98,6 @@ static bool isConstZero(Value v) {
 /// Optimize padding on L1 if we're given the hint.
 void tryOptimizePad(Operation *maybeLoadOp, Value mmadSource,
                     PatternRewriter &rewriter) {
-  assert(!isa<BlockArgument>(mmadSource));
   bool padKOnly =
       utils::getAnnotateOpWithAttr(mmadSource, kDotPadOnlyK).has_value();
   if (!padKOnly)
