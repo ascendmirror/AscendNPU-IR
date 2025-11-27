f054564b0d0178cc7f5e716b547e8386567e9e88
the_cdd <13028259916@163.com>
2025-11-27 22:42:56 +0800

    [AscendNPU IR] feat: fixpipeop support nz2dn [AscendNPU IR] feat: fixpipeop support nz2dn
    Offering: BiSheng Enterprise AscendNPU IR

--- bishengir/lib/Dialect/HIVM/IR/HIVMDMAOps.cpp
+++ bishengir/lib/Dialect/HIVM/IR/HIVMDMAOps.cpp
@@ -565,6 +565,7 @@ void FixpipeOp::build(OpBuilder &odsBuilder, OperationState &odsState,
                       UnitAttr enable_nz2nd,
 #else
                       FixpipeDMAModeAttr dma_mode,
+                      FixpipeDualDstModeAttr dual_mode,
 #endif // BISHENGIR_ENABLE_A5_UNPUBLISHED_FEATURES
                       FixpipePreQuantModeAttr pre_quant,
                       FixpipePreReluModeAttr pre_relu, BoolAttr channel_split) {
@@ -585,6 +586,7 @@ void FixpipeOp::build(OpBuilder &odsBuilder, OperationState &odsState,
                       UnitAttr enable_nz2nd,
 #else
                       FixpipeDMAModeAttr dma_mode,
+                      FixpipeDualDstModeAttr dual_mode,
 #endif // BISHENGIR_ENABLE_A5_UNPUBLISHED_FEATURES
                       FixpipePreQuantModeAttr pre_quant,
                       FixpipePreReluModeAttr pre_relu, BoolAttr channel_split) {
